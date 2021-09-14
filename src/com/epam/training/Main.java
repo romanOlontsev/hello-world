ab6b59beb53428662e44dd83ccca4c4c167bb829
romanOlontsev <roverviner@yandex.ru>
2021-09-14 11:20:15 +0300

    my first program

---
+++ src/com/epam/training/Main.java
@@ -0,0 +1,9 @@
+package com.epam.training;
+
+public class Main {
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
